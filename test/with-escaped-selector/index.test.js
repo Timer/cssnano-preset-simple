878a1a8e29535f3e28e1d8a345d07052b1199401
Joe Haddad <timer150@gmail.com>
2021-07-06 11:52:06 -0400

    feat: upgrade cssnano
    BREAKING CHANGE: This change increases the minimum version of PostCSS required, so it is a major.
    
    close #19

--- test/with-escaped-selector/index.test.js
+++ test/with-escaped-selector/index.test.js
@@ -14,6 +14,6 @@ describe('with escaped selector', () => {
       to: 'output.css',
     });
 
-    expect(res.css).toMatchInlineSnapshot(`".foo\\\\,2{background:#00f}"`);
+    expect(res.css).toMatchInlineSnapshot(`".foo\\\\,2{background:blue}"`);
   });
 });

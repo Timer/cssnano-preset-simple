353fecb87364affa76f8743e92b571f1951feb3f
Joe Haddad <timer@vercel.com>
2022-03-12 05:35:49 +0000

    fix: upgrade cssnano-preset-default and related deps
    BREAKING CHANGE: cssnano-preset-default now returns an esModule output instead of cjs

--- patch.js
+++ patch.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 const pkg = require.resolve('browserslist/node');
-const replaceValue = `var queries = require(require.resolve(name, { paths: ['.'] }))`;
-const newValue = `var queries = __non_webpack_require__(require.resolve(name, { paths: ['.'] }))`;
+const replaceValue = `var queries = require(require.resolve(name, { paths: ['.', ctx.path] }))`;
+const newValue = `var queries = __non_webpack_require__(require.resolve(name, { paths: ['.', ctx.path] }))`;
 
 let contents = fs.readFileSync(pkg, 'utf8');
 
